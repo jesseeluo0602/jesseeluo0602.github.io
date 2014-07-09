8dc55983f5628be60a5e77a4604480dcb55ff3dd
unknown <jesseeluo0602@gmail.com>
2014-07-08 18:00:02 -0700

    first commit

---
+++ controllers.js
@@ -0,0 +1,13 @@
+  var app = angular.module('phonecatApp', []);
+
+  app.controller('PhoneController', function(){
+    this.phones = phoneList;
+  });
+
+  var phoneList = [
+    { name: 'Holy Dragon', rank: "A",  type:"Fire", imageUrl:"http://img2.wikia.nocookie.net/__cb20131222134656/quiz-rpg-the-world-of-mystic-wiz/images/6/61/Holy_Dragon_Icon.png", wiki:"http://quiz-rpg-the-world-of-mystic-wiz.wikia.com/wiki/Holy_Dragon" },
+    { name: 'White Flame Holy Dragon', rank: "A-",  type:"Fire", imageUrl:"http://img1.wikia.nocookie.net/__cb20131222134724/quiz-rpg-the-world-of-mystic-wiz/images/thumb/3/33/White_Flame_Holy_Dragon_Icon.png/100px-White_Flame_Holy_Dragon_Icon.webp", wiki:"http://quiz-rpg-the-world-of-mystic-wiz.wikia.com/wiki/White_Flame_Holy_Dragon"},
+    { name: 'Fiery Dragon', rank: "B", type:"Fire", imageUrl:"http://img4.wikia.nocookie.net/__cb20131222112237/quiz-rpg-the-world-of-mystic-wiz/images/thumb/5/5f/Fiery_Dragon_Icon.png/100px-Fiery_Dragon_Icon.png", wiki:"http://quiz-rpg-the-world-of-mystic-wiz.wikia.com/wiki/Fiery_Dragon" },
+    { name: 'Riel', rank: "A", type:"Water", imageUrl:"http://img2.wikia.nocookie.net/__cb20140106070142/quiz-rpg-the-world-of-mystic-wiz/images/thumb/9/9e/Riel_%28Aqua_Pistol%29_Icon.png/100px-Riel_%28Aqua_Pistol%29_Icon.webp", wiki:"http://quiz-rpg-the-world-of-mystic-wiz.wikia.com/wiki/Riel_(Aqua_Pistol)" },
+
+  ];
